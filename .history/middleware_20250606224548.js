d172ebf57a0575de1dabd25fb2556e10d0e218de
Your Name <walaa.horani@hotmail.com>
2025-06-06 22:50:53 +0300

    first commit

--- .history/middleware_20250606224548.js
+++ .history/middleware_20250606224548.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 /** @param {import('next/server').NextRequest} request */
 export function middleware(request) {
   const token = request.cookies.get('__Secure-kinde-access-token');
-
+  
   if (!token) {
     return NextResponse.redirect(
       new URL('/api/auth/login?post_login_redirect_url=/dashboard', request.url)
